b05ad87009a556caa8c8295b8788807232339763
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2023-11-08 17:41:21 +0000

    better swipe behaviour

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let type = "chevron";
+  export let rotation = 0;
+  export let position = "inline";
+  export let clickable = false;
+  export let margin = false;
+
+  const paths = {
+      chevron: "M10 6 8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z",
+      arrow: "M5,13h11.2l-2.9,2.9c-0.4,0.4-0.4,1,0,1.4s1,0.4,1.4,0l4.6-4.6c0.4-0.4,0.4-1,0-1.4l-4.6-4.6c-0.4-0.4-1-0.4-1.4,0c0,0,0,0,0,0c-0.4,0.4-0.4,1,0,1.4l2.9,2.9H5c-0.6,0-1,0.4-1,1S4.4,13,5,13z",
+    };
+
+  $: style = `transform: translate(0, 15%) scale(1.5) rotate(${-rotation}deg);${["left", "right"].includes(position) ? ` position: absolute; ${position}: 10px;` : ''}`;
+</script>
+
+<svg
+  class="ico"
+  viewBox="0 0 24 24"
+  fill-rule="evenodd"
+  clip-rule="evenodd"
+  aria-hidden="true"
+  focusable="false"
+  {style}
+  class:margin
+  class:noclick={!clickable}>
+  <path d={paths[type]}></path>
+</svg>
+
+<style>
+  .ico {
+    position: inherit;
+    width: 1em;
+    height: 1em;
+    fill: currentColor;
+    overflow: visible;
+    transition: all 0.3s ease-out;
+  }
+  .noclick {
+    pointer-events: none;
+  }
+  .margin {
+    margin: 0 6px 0 1px;
+  }
+</style>
--- src/routes/[direction]/+page.svelte
+++ src/routes/[direction]/+page.svelte
@@ -2,66 +2,86 @@
   // @ts-nocheck
   import "../../app.css";
   import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
   import { base } from "$app/paths";
+  import { Swipe, SwipeItem } from "svelte-swipe";
+  import Icon from "$lib/Icon.svelte";
 
   export let data;
 
-  let destination;
+  let swipe;
+  let destination = $page.params.direction;
+
+  const swipeConfig = {
+    autoplay: false,
+    transitionDuration: 500,
+    defaultIndex: 0,
+  };
 </script>
 
 <svelte:head>
   <title>Rob's next train to London</title>
 </svelte:head>
 
-<div class="selector">
-  <label>
-    Destination
-    <select bind:value={destination} on:change={() => goto(`${base}/${destination}`)}>
-      <option value="out">London Waterloo</option>
-      <option value="rtn">Southampton Airport Parkway</option>
-    </select>
-  </label>
-</div>
-<div class="container" style:width="{data.trainServices.length * 400 + (data.trainServices.length - 1) * 3}px">
-  {#each data.trainServices as train, i}
-  <div class="train" class:first={i === 0} class:cancelled={train.etd === "Cancelled"} class:noplatform={!train.platform && train.etd !== "Cancelled"}>
-    <div class="header">
-      <div class="text-lg flex">
-        <span>{train.std}</span>
-        {#if train.etd === "Cancelled"}
-        <span class="yellow">Cancelled</span>
-        {:else if train.etd !== "On time"}
-        <span>Expt {train.etd}</span>
-        {/if}
-      </div>
-      <div class="text-xl dest-name">
-        {train.destination.name}
-      </div>
-      <div class="text-md" style:margin="6px 0 -4px">
-        {@html train.etd !== "Cancelled" ? 'Platform' : '&nbsp;'}
-      </div>
-      <div class="text-xl">
-        {@html train.etd !== "Cancelled" ? train.platform || "Wait" : '&nbsp;'}
-      </div>
-    </div>
-    <div class="body">
-      <div class="text-sm">
-        <div class="yellow">{train.etd !== "Cancelled" ? 'Calling at:' : 'Was calling at:'}</div>
-        {#each train.subsequentCallingPoints as station}
-          <div class="flex" style:font-weight="{["WAT", "SOA"].includes(station.crs) ? "bold" : null}">
-            <span>{station.locationName}</span>
-            {#if train.etd !== "Cancelled"}
-            <span>{station.et === "On time" ? station.st : station.et}</span>
+<div class="container">
+  <div class="selector">
+    <label>
+      <span>Destination</span>
+      <select bind:value={destination} on:change={() => goto(`${base}/${destination}`)}>
+        <option value="out">London Waterloo</option>
+        <option value="rtn">Southampton Airport Parkway</option>
+      </select>
+    </label>
+  </div>
+  {#key data}
+  <div class="next-prev">
+    <button title="Previous" on:click={() => swipe.prevItem()}><Icon rotation="180"/></button>
+    <button title="Next" on:click={() => swipe.nextItem()}><Icon/></button>
+  </div>
+  <Swipe {...swipeConfig} bind:this={swipe}>
+    {#each data.trainServices as train, i}
+    <SwipeItem>
+      <div class="train" class:cancelled={train.etd === "Cancelled"} class:noplatform={!train.platform && train.etd !== "Cancelled"}>
+        <div class="header">
+          <div class="text-lg flex">
+            <span>{train.std}</span>
+            {#if train.etd === "Cancelled"}
+            <span class="yellow">Cancelled</span>
+            {:else if train.etd !== "On time"}
+            <span>Expt {train.etd}</span>
             {/if}
           </div>
-        {/each}
-      </div>
-      <div>
-        <div style:margin-top="36px">
-          Operated by {train.operator}
+          <div class="text-xl dest-name">
+            {train.destination.name}
+          </div>
+          <div class="text-md" style:margin="6px 0 -4px">
+            {@html train.etd !== "Cancelled" ? 'Platform' : '&nbsp;'}
+          </div>
+          <div class="text-xl">
+            {@html train.etd !== "Cancelled" ? train.platform || "Wait" : '&nbsp;'}
+          </div>
+        </div>
+        <div class="body">
+          <div class="text-sm">
+            <div class="yellow">{train.etd !== "Cancelled" ? 'Calling at:' : 'Was calling at:'}</div>
+            {#each train.subsequentCallingPoints as station}
+              <div class="flex" style:font-weight="{["WAT", "SOA"].includes(station.crs) ? "bold" : null}">
+                <span>{station.locationName}</span>
+                {#if train.etd !== "Cancelled"}
+                <span>{station.et === "On time" ? station.st : station.et}</span>
+                {/if}
+              </div>
+            {/each}
+          </div>
+          <div>
+            <div style:margin-top="36px">
+              Operated by {train.operator}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  {/each}
+    </SwipeItem>
+    {/each}
+  </Swipe>
+  {/key}
 </div>
